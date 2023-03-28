c21642058fb8bc55a2ac334d12cbba7ec22bdfb1
Sergio Molina Cardín <sergio.molina_cardin.ext@nokia.com>
2023-03-28 15:58:00 +0200

    Added setExecChFromMain() function to save the function pointer to call execute challenge from main thread.

--- context_challenge.h
+++ context_challenge.h
@@ -16,10 +16,13 @@
 
 struct ChallengeEquivalenceGroup* group = NULL;
 struct Challenge* challenge = NULL;
-BOOL periodic_execution = TRUE;
+bool periodic_execution = true;
 int validity_time = 0;
-int refresh_time = 0;
+int refresh_time = INT_MAX;
 HANDLE h_thread = INVALID_HANDLE_VALUE;
+
+typedef int(__stdcall* ExecChFromMain_func_type)(struct ChallengeEquivalenceGroup*, struct Challenge*);
+ExecChFromMain_func_type execChFromMain = NULL;
 
 
 
@@ -29,6 +32,7 @@ HANDLE h_thread = INVALID_HANDLE_VALUE;
 extern "C" _declspec(dllexport) int init(struct ChallengeEquivalenceGroup* group_param, struct Challenge* challenge_param);
 extern "C" _declspec(dllexport) int executeChallenge();
 extern "C" _declspec(dllexport) void setPeriodicExecution(bool active_param);
+extern "C" _declspec(dllexport) void setExecChFromMain(ExecChFromMain_func_type p_fun);
 void launchPeriodicExecution();
 void refreshSubkey(LPVOID th_param);
 
@@ -64,25 +68,31 @@ struct KeyData {
 /////  FUNCTION IMPLEMENTATIONS  /////
 
 void setPeriodicExecution(bool active_param) {
-	printf("--- Periodic exec: %d \n", active_param);
+	printf("--- Setting periodic_execution: %s \n", active_param ? "true" : "false");
 	periodic_execution = active_param;
 }
 
 void launchPeriodicExecution() {
-	if (periodic_execution) {
+	if (periodic_execution && h_thread == INVALID_HANDLE_VALUE) {
 		h_thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)refreshSubkey, NULL, 0, NULL);
 	}
 }
 
 void refreshSubkey(LPVOID th_param) {
 	Sleep(refresh_time * 1000);
+
 	while (periodic_execution) {
-		executeChallenge();
-		Sleep(refresh_time * 1000);		// Sleep first due to execute function already launched by init()
+		printf("--- Periodic challenge execution for key refreshing...\n");
+		//executeChallenge();
+		execChFromMain(group, challenge);
+		Sleep(refresh_time * 1000);      // Sleep first due to execute function already launched by init()
 	}
+	h_thread = INVALID_HANDLE_VALUE;
 	return;
 }
 
-
+void setExecChFromMain(ExecChFromMain_func_type p_fun) {
+	execChFromMain = p_fun;
+}
 
 #endif // !CONTEXT_CHALLENGE_H
